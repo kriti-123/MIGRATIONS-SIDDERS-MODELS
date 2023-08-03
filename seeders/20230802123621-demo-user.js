c3895126708f2e64446d45a441dd0d5116a128de
kriti-123 <ks11115500@gmail.com>
2023-08-03 11:37:33 +0530

    initial commit

---
+++ seeders/20230802123621-demo-user.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('students', [{
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'example@example.com',
+      empId:'ksg',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
